3e8745e829e76079dedfc96cfecf5474f4b92e5e
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-11-03 10:09:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,10 @@ echo "<br>";
 	if ($t == 1 and $r ==1){
 		echo  "Almost snake eyes";
 		}
-	elseif ($t == 1 xor $r<1)
-				elseif ($t == 6 and $r == 6){ 
+	elseif ($t == 1 xor $r<1){
+		echo "Snake eyes";
+	}	
+			elseif ($t == 6 and $r == 6){ 
 		echo "Winner";
 		
 	}
