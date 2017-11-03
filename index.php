d2c72cc8fb236391cb0e3fe50bb500e641cbf9ef
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-11-03 09:42:57 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$t = rand(1,6);
+echo $t;
+
+	if ($t==1){
+		echo  "Almost snake eyes";
+		}
+	
+	elseif ($t==6){ 
+		echo "Winner";
+		
+	}
+	
+	else {
+		echo "you lose";
+	}
+	
+	endif;
+?>
+
+</body>
+</html>
