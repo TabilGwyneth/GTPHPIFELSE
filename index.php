c984d65b03b81be4a74b59dab814c0f126e58517
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-11-03 09:40:50 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 <?php
 
 $t = rand(1,6); 
+echo $t; 
   
-if ($t == "1"){  
+if ($t == 1){  
   echo  "Almost snake eyes";  
-} elseif ($t == "6" ){   
+} elseif ($t == 6 ){   
   echo "winner";  
 } else {
   echo "you lost";
